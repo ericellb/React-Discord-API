835df5e644a6709b14e9adab69a9ca318e693d42
shmeegie <ericellb@gmail.com>
2019-08-06 03:58:12 -0400

    Added env port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3001;
+
 app.get('/', function (req, res) {
   res.send('<h1>Hello world</h1>');
 });
@@ -14,6 +16,6 @@ io.on('connection', function (socket) {
   });
 });
 
-http.listen(3001, function () {
-  console.log('listening on :3001');
+http.listen(PORT, function () {
+  console.log(`listening on ${PORT}`);
 });
