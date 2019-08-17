98ffa848c1599255acdaa2a82779a8e3a7d8a8de
shmeegie <ericellb@gmail.com>
2019-08-17 01:07:19 -0400

    Rename + Delete channel rotues

--- src/routes/channel.js
+++ src/routes/channel.js
@@ -29,11 +29,61 @@ router.post('/channel/create', async (req, res) => {
   }
 });
 
+// Route to rename a channel
+// Expects -> Channel Name
+// Expects -> Channel Id
+// Expects -> Userid
+router.post('/channel/rename', async (req, res) => {
+  const { channelName, channelId, serverId, userId } = req.query;
+  if (!channelName || !channelId || !userId || !serverId) {
+    res.status(400).send('Invalid Params');
+  }
+  else {
+    // Check if user admin
+    if (await userIsAdmin(userId, serverId)) {
+      renameChannel(channelName, channelId);
+      res.status(200).send(`Renamed Channel to ${channelName}`);
+    }
+    else {
+      res.status(401).send("Not authorized, stop that!");
+    }
+  }
+})
+
+// Route to delete a channel
+// Expects -> ChannelId
+// Expects -> UserId
+router.delete('/channel/delete', async (req, res) => {
+  const { channelId, serverId, userId } = req.query;
+  if (!channelId || !userId) {
+    res.status(400).send('Invalid Params');
+  }
+  else {
+    // Check if user admin
+    if (await userIsAdmin(userId, serverId)) {
+      deleteChannel(channelId);
+      res.status(200).send(`Deleted channel`);
+    }
+    else {
+      res.status(401).send("Not authorized, stop that!");
+    }
+  }
+})
+
 
 // Create channel and all intermediary tables
 const createChannel = (channelId, channelName, serverId, userId) => {
   sql.query(`INSERT INTO channels (channel_id, channel_name, server_id) VALUES ('${channelId}', '${channelName}', '${serverId}')`);
   sql.query(`INSERT INTO messages (channel_id) VALUES ('${channelId}')`);
+}
+
+// Remane a channel
+const renameChannel = (channelName, channelId) => {
+  sql.query(`UPDATE channels SET channel_name = '${channelName}' WHERE channel_id = '${channelId}'`);
+}
+
+const deleteChannel = (channelId) => {
+  sql.query(`DELETE FROM channels WHERE channel_id = '${channelId}'`);
 }
 
 module.exports = router;
