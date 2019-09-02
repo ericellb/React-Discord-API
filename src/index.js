20357b999c90493968014967f44e28e51cbd41c4
shmeegie <ericellb@gmail.com>
2019-09-02 18:54:14 -0400

    Small fixes

--- src/index.js
+++ src/index.js
@@ -19,75 +19,82 @@ async function main() {
   let clients = [];
 
   // Create socket
-  io.on('connection', function (socket) {
-
+  io.on('connection', function(socket) {
     // Keep track of current socket userId
     let sessionUserId = null;
 
     // Listens for new messages
-    socket.on('simple-chat-message', async (msg) => {
+    socket.on('simple-chat-message', async msg => {
       // Store messsage in the DB
       var date = new Date();
-      let sqlQuery = `INSERT INTO messages (channel_id, user_name, msg, date_time) VALUES (${sql.escape(msg.channel.split('-')[1])}, ${sql.escape(msg.from)}, ${sql.escape(msg.msg)}, ${sql.escape(date)})`;
+      let sqlQuery = `INSERT INTO messages (channel_id, user_name, msg, date_time) VALUES (${sql.escape(
+        msg.channel.split('-')[1]
+      )}, ${sql.escape(msg.from)}, ${sql.escape(msg.msg)}, ${sql.escape(date)})`;
       sql.query(sqlQuery);
       const serverId = msg.server.split('-')[1];
 
       // Format our action for client to parse
-      action = { type: "message", payload: msg };
+      action = { type: 'message', payload: msg };
 
       // Emit the message to everyone that joined that server
       io.to(serverId).emit('update', action);
     });
 
-
     // Listens for private messages
-    socket.on('simple-chat-private-message', async (message) => {
-
+    socket.on('simple-chat-private-message', async message => {
       // Find userId for username we are messaging
       const from = await sql.query(`SELECT user_id from users WHERE user_name = ${sql.escape(message.from)}`);
       const to = await sql.query(`SELECT user_id from users WHERE user_name = ${sql.escape(message.to)}`);
       let date = new Date();
-      sql.query(`INSERT INTO user_messages (user_from, user_to, msg, date_time) VALUES (${sql.escape(from[0].user_id)}, ${sql.escape(to[0].user_id)}, ${sql.escape(message.msg)}, ${sql.escape(date)})`);
+      sql.query(
+        `INSERT INTO user_messages (user_from, user_to, msg, date_time) VALUES (${sql.escape(
+          from[0].user_id
+        )}, ${sql.escape(to[0].user_id)}, ${sql.escape(message.msg)}, ${sql.escape(date)})`
+      );
 
       // Find which socket to send TO
-      let action = { type: "private-message", payload: { from: message.from, to: message.to, msg: message.msg, user: message.from.toLowerCase() } };
-      clients.find((client) => {
+      let action = {
+        type: 'private-message',
+        payload: { from: message.from, to: message.to, msg: message.msg, user: message.from.toLowerCase() }
+      };
+      clients.find(client => {
         if (client.userId === to[0].user_id) {
           io.to(client.id).emit('update', action);
         }
-      })
+      });
 
-      // Find which socket to 
-      action = { type: "private-message", payload: { from: message.from, to: message.to, msg: message.msg, user: message.to.toLowerCase() } };
-      clients.find((client) => {
+      // Find which socket to
+      action = {
+        type: 'private-message',
+        payload: { from: message.from, to: message.to, msg: message.msg, user: message.to.toLowerCase() }
+      };
+      clients.find(client => {
         if (client.userId === from[0].user_id) {
           io.to(client.id).emit('update', action);
         }
-      })
+      });
     });
-
 
     // When user signs in he sends over his userId
     // Add to list of clients userId to identify socket.id
-    socket.on('simple-chat-sign-in', (userId) => {
+    socket.on('simple-chat-sign-in', userId => {
       // Keep track of session userId to eventually remove from list of clients
       sessionUserId = userId;
       clients.push({ userId: sessionUserId, id: socket.id });
       let date = new Date();
       sql.query(`UPDATE users SET user_last_active = ${sql.escape(date)} WHERE user_id = ${sql.escape(userId)}`);
-    })
-
+    });
 
     // Listens for subscribing to servers (socket io rooms)
-    socket.on('subscribe', (serverId) => {
+    socket.on('subscribe', serverId => {
       socket.join(serverId);
-    })
+    });
 
     // On ping update active status (Client sends every 5 minutes)
     socket.on('ping', () => {
       let date = new Date();
       sql.query(`UPDATE users SET user_last_active = ${sql.escape(date)} WHERE user_id = ${sql.escape(sessionUserId)}`);
-    })
+    });
 
     // On disconnect remove from client list
     socket.on('disconnect', () => {
@@ -95,15 +102,12 @@ async function main() {
         if (client.userName === sessionUserId) {
           return clients.splice(i, 1);
         }
-      })
-    })
-
-
+      });
+    });
   });
 
-
   // Server listen on Port
-  http.listen(PORT, function () {
+  http.listen(PORT, function() {
     console.log(`API Listening on ${PORT}`);
   });
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -11,90 +11,88 @@ router.get('/user/data', async (req, res) => {
   const { userId } = req.query;
   const data = {};
   // Query to get all of the servers + channels + data
-  await sql.query(`SELECT servers.server_id, servers.server_name, channels.channel_id, channels.channel_name, messages.user_name, messages.msg, messages.date_time 
+  await sql.query(
+    `SELECT servers.server_id, servers.server_name, channels.channel_id, channels.channel_name, messages.user_name, messages.msg, messages.date_time 
   FROM messages
   right JOIN channels ON messages.channel_id = channels.channel_id
   JOIN servers ON servers.server_id = channels.server_id
   JOIN userservers ON servers.server_id = userservers.server_id
   JOIN users ON userservers.user_id = users.user_id
-  WHERE users.user_id = ${sql.escape(userId)}`, (err, result) => {
+  WHERE users.user_id = ${sql.escape(userId)}`,
+    (err, result) => {
       if (err) {
         res.status(400).send('Server error');
         throw err;
-      }
-      else {
-        result.forEach((message) => {
+      } else {
+        result.forEach(message => {
           // Build servers / channel names with IDs
           const serverName = message.server_name + '-' + message.server_id;
           const channelName = message.channel_name + '-' + message.channel_id;
 
-          if (data["servers"] === undefined)
-            data["servers"] = {};
+          if (data['servers'] === undefined) data['servers'] = {};
 
-          if (data["servers"][serverName] === undefined)
-            data["servers"][serverName] = {};
+          if (data['servers'][serverName] === undefined) data['servers'][serverName] = {};
 
-          if (data["servers"][serverName]["activeUsers"] === undefined)
-            data["servers"][serverName]["activeUsers"] = [];
+          if (data['servers'][serverName]['channels'] === undefined) data['servers'][serverName]['channels'] = {};
 
-          if (data["servers"][serverName]["channels"] === undefined)
-            data["servers"][serverName]["channels"] = {};
-
-          if (data["servers"][serverName]["channels"][channelName] === undefined)
-            data["servers"][serverName]["channels"][channelName] = [];
+          if (data['servers'][serverName]['channels'][channelName] === undefined)
+            data['servers'][serverName]['channels'][channelName] = [];
 
           if (message.user_name !== null && message.msg !== null)
-            data["servers"][serverName]["channels"][channelName].push({ "from": message.user_name, "msg": message.msg, "date": message.date_time });
-        })
-
+            data['servers'][serverName]['channels'][channelName].push({
+              from: message.user_name,
+              msg: message.msg,
+              date: message.date_time
+            });
+        });
       }
 
       // Query to get all Private messages for user
-      sql.query(`SELECT b.user_name as user_from, c.user_name as user_to, msg 
+      sql.query(
+        `SELECT b.user_name as user_from, c.user_name as user_to, msg 
                 FROM user_messages a
                 JOIN users b ON b.user_id = a.user_from 
                 JOIN users c ON c.user_id = a.user_to 
                 WHERE a.user_from = ${sql.escape(userId)} 
-                OR a.user_to = ${sql.escape(userId)}`, async (err, result) => {
+                OR a.user_to = ${sql.escape(userId)}`,
+        async (err, result) => {
           if (err) {
             res.status(400).send('Server error');
             throw err;
-          }
-          else {
-            const userName = await sql.query(`SELECT user_name from users where user_id = ${sql.escape(userId)}`)
-            result.forEach((privateMessage) => {
+          } else {
+            const userName = await sql.query(`SELECT user_name from users where user_id = ${sql.escape(userId)}`);
+            result.forEach(privateMessage => {
               // Build privateMessages object
               let user = null;
 
+              // If messages from me, set user to the TO
+              if (privateMessage.user_from === userName[0].user_name) user = privateMessage.user_to.toLowerCase();
+              else user = privateMessage.user_from.toLowerCase();
 
-              // If messages from me, set user to the TO 
-              if (privateMessage.user_from === userName[0].user_name)
-                user = privateMessage.user_to.toLowerCase();
-              else
-                user = privateMessage.user_from.toLowerCase();
+              if (data['privateMessages'] === undefined) data['privateMessages'] = {};
 
-              if (data["privateMessages"] === undefined)
-                data["privateMessages"] = {};
+              if (data['privateMessages'][user] === undefined) data['privateMessages'][user] = [];
 
-              if (data["privateMessages"][user] === undefined)
-                data["privateMessages"][user] = [];
-
-              data["privateMessages"][user].push({ "user": user, "from": privateMessage.user_from, "to": privateMessage.user_to, "msg": privateMessage.msg, "date_time": privateMessage.date_time });
-            })
+              data['privateMessages'][user].push({
+                user: user,
+                from: privateMessage.user_from,
+                to: privateMessage.user_to,
+                msg: privateMessage.msg,
+                date_time: privateMessage.date_time
+              });
+            });
 
             // If no private messages return data with empty privateMessages array
-            if (data["privateMessages"] === undefined)
-              data["privateMessages"] = [];
+            if (data['privateMessages'] === undefined) data['privateMessages'] = [];
           }
 
           // Return our final formatted data
-          res.status(200).send(data)
-        })
-
-    })
-
+          res.status(200).send(data);
+        }
+      );
+    }
+  );
 });
-
 
 // Route to create a new user
 // Expects -> userName
@@ -107,21 +105,19 @@ router.post('/user/create', async (req, res) => {
 
   // Check for errors
   if (!userName || !userPass) {
-    error = "Invalid Params";
-  }
-  else if (userPass.length < 6) {
-    error = "Passwords need to be minimum 6 chracters";
+    error = 'Invalid Params';
+  } else if (userPass.length < 6) {
+    error = 'Passwords need to be minimum 6 chracters';
   }
 
   // If errors, send them
   if (error !== null) {
     res.status(401).send(error);
-  }
-  else {
+  } else {
     let response = await sql.query(`SELECT user_id from users where user_name = ${sql.escape(userName)}`);
     // User already exists
     if (response.length > 0) {
-      error = "Username already exists";
+      error = 'Username already exists';
       res.status(401).send(error);
     }
     // No user exists, lets create it!
@@ -133,15 +129,19 @@ router.post('/user/create', async (req, res) => {
           userPass = hash;
           var date = new Date();
           // Create user
-          sql.query(`INSERT INTO users (user_id, user_name, user_pass, user_last_active) VALUES (${sql.escape(userId)}, ${sql.escape(userName)}, ${sql.escape(userPass)}, ${sql.escape(date)})`);
+          sql.query(
+            `INSERT INTO users (user_id, user_name, user_pass, user_last_active) VALUES (${sql.escape(
+              userId
+            )}, ${sql.escape(userName)}, ${sql.escape(userPass)}, ${sql.escape(date)})`
+          );
           // Add to default server
           sql.query(`INSERT INTO userservers (server_id, user_id) VALUES ('FANfDprXmt', '${userId}')`);
-          res.status(200).send({ "userName": userName, "userId": userId });
-        })
-      })
+          res.status(200).send({ userName: userName, userId: userId });
+        });
+      });
     }
   }
-})
+});
 
 // Route to login
 // Expects -> userName
@@ -161,13 +161,11 @@ router.get('/user/login', async (req, res) => {
   const hashPass = response[0].user_pass;
   const isMatch = await bcrypt.compare(userPass, hashPass);
   if (isMatch) {
-    res.status(200).send({ "userName": userName, "userId": response[0].user_id });
-  }
-  else {
+    res.status(200).send({ userName: userName, userId: response[0].user_id });
+  } else {
     error = 'Username / Password does not match';
     res.status(400).send(error);
   }
 });
-
 
 module.exports = router;
